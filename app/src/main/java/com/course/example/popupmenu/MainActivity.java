231acc14960751d4450f06e4f62bcfd11d8ce9ce
Android Wonder <jim.pepe@verizon.net>
2022-05-04 13:52:09 -0400

    reformat code

--- app/src/main/java/com/course/example/popupmenu/MainActivity.java
+++ app/src/main/java/com/course/example/popupmenu/MainActivity.java
@@ -39,48 +39,51 @@ public class MainActivity extends AppCompatActivity {
 
                 //set listener for when menu item clicked
                 popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                                                         @Override
-                                                         public boolean onMenuItemClick(MenuItem menuItem) {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
 
-                                                             String selectItem = menuItem.getTitle().toString();
-                                                             int position = 0;
+                        String selectItem = menuItem.getTitle().toString();
+                        int position = 0;
 
-                                                             //get position clicked
-                                                             switch (menuItem.getItemId()) {
-                                                                 case R.id.first:
-                                                                     position = 0;
-                                                                     break;
-                                                                 case R.id.second:
-                                                                     position = 1;
-                                                                     break;
-                                                                 case R.id.third:
-                                                                     position = 2;
-                                                                     break;
-                                                                 case R.id.fourth:
-                                                                     position = 3;
-                                                                     break;
-                                                                 case R.id.fifth:
-                                                                     position = 4;
-                                                                     break;
-                                                             }
+                        //get position clicked
+                        switch (menuItem.getItemId()) {
+                            case R.id.first:
+                                position = 0;
+                                break;
+                            case R.id.second:
+                                position = 1;
+                                break;
+                            case R.id.third:
+                                position = 2;
+                                break;
+                            case R.id.fourth:
+                                position = 3;
+                                break;
+                            case R.id.fifth:
+                                position = 4;
+                                break;
+                        }
 
-                                                             //open toast
-                                                             Toast.makeText(MainActivity.this, selectItem, Toast.LENGTH_LONG).show();
-                                                             //get url for item clicked
-                                                             urlPass = Urls.urls[position];
+                        //open toast with selection
+                        Toast.makeText(MainActivity.this, selectItem, Toast.LENGTH_LONG).show();
 
-                                                             //open browser
-                                                             Uri uri2 = Uri.parse(urlPass);
-                                                             Intent i2 = new Intent(Intent.ACTION_VIEW,uri2);
-                                                             startActivity(i2);
-                                                             return true;
-                                                         };
+                        //get url for item clicked
+                        urlPass = Urls.urls[position];
 
-                                                     });
+                        //open browser
+                        Uri uri2 = Uri.parse(urlPass);
+                        Intent i2 = new Intent(Intent.ACTION_VIEW, uri2);
+                        startActivity(i2);
+                        return true;
+                    }
+
+                    ;
+
+                });
                 // Showing the popup menu
                 popupMenu.show();
             }
         });
     } //onCreate
-    
+
 }
